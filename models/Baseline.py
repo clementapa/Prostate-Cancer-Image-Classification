776050f6aff4ee150ce67f04ce8f97cfd64c13da
younesbelkada <younesbelkada@gmail.com>
2022-03-04 10:56:52 +0100

    fix baseline

--- models/Baseline.py
+++ models/Baseline.py
@@ -14,22 +14,23 @@ class Baseline(nn.Module):
 
         self.features_extractor = timm.create_model(
             self.params.feature_extractor_name, pretrained=True
-        )  # TODO how deal with the input?
+        )
+        self.features_extractor.reset_classifier(0)
+        in_shape = self.features_extractor(torch.randn(1, 3, 224, 224)).shape[1]
 
         self.mlp = MLP(
-            self.features_extractor.fc.in_features * params.nb_samples, params
+            in_shape * params.nb_samples, params
         )
 
     def forward(self, x):
-        # step 1: random sampling patches
         features = []
         for batch in x:
-            # features.append(self.features_extractor(batch.permute(0, 3, 1, 2)))
-            features.append(
-                self.features_extractor.forward_features(
-                    batch.permute(0, 3, 1, 2)
-                ).squeeze()
-            )
+            features.append(self.features_extractor(batch.permute(0, 3, 1, 2)))
+            # features.append(
+            #     self.features_extractor.forward_features(
+            #         batch.permute(0, 3, 1, 2)
+            #     ).squeeze()
+            # )
         # bs, n_patches, h, w, c
         features = torch.stack(features)
 
