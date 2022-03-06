0bb15cf7290e13ec5fcf3e9c60234242f6db34a5
Clement Apavou <apavou.clement@outlook.fr>
2022-03-06 13:07:51 +0100

    tune batch size false default

--- config/hparams.py
+++ config/hparams.py
@@ -33,7 +33,7 @@ class Hparams:
 
     # modes
     tune_lr: bool = False  # tune the model on first run
-    tune_batch_size: bool = True
+    tune_batch_size: bool = False
     dev_run: bool = False
     train: bool = True
 
