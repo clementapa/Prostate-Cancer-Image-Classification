a4fb684067ad45dbf12969650b38cbb133d8ec8b
Clement Apavou <apavou.clement@outlook.fr>
2022-03-08 16:31:10 +0100

    remove data augmentation

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -113,9 +113,6 @@ class PatchSegDataset(BaseSegDataset):
 
         self.transform = transforms.Compose(
             [
-                transforms.RandomHorizontalFlip(),
-                transforms.RandomVerticalFlip(),
-                transforms.ColorJitter(0.1, 0.1, 0.1, 0.01),
                 transforms.ToTensor(),
                 transforms.Normalize(
                     mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
