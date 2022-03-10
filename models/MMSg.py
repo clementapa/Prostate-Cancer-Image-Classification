a8a8bd5f23f04fceb864fa8facd8a72dcbfe9618
younesbelkada <younesbelkada@gmail.com>
2022-03-10 16:38:21 +0100

    correct activation
    Co-authored-by: APAVOU Clément <clementapa@users.noreply.github.com>

--- models/MMSg.py
+++ models/MMSg.py
@@ -36,7 +36,7 @@ class MMSg(nn.Module):
         elif self.params.classifier_name == "Multiple Linear":
                 self.classifier = nn.Sequential(
                                     nn.Linear(in_shape+4, (in_shape+4)//2),
-                                    self.activation(),
+                                    getattr(nn, params.activation)(),
                                     nn.Linear((in_shape+4)//2, 6),
                                 )
         else:
