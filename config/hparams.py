abec2dd52f08c33de93d5ec06d61f13d768829e9
younesbelkada <younesbelkada@gmail.com>
2022-03-28 10:52:08 +0200

    resnet50

--- config/hparams.py
+++ config/hparams.py
@@ -119,7 +119,7 @@ class CallbacksParams:
 
 @dataclass
 class NetworkClassificationParams:
-    feature_extractor_name: str = "resnet152"
+    feature_extractor_name: str = "resnet50"
     network_name: str = "SimpleModel"
 
 
