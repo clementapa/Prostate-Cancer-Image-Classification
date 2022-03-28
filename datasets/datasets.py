8192eda8cd0aeb051a392810be7fea5eaa89bb01
younesbelkada <younesbelkada@gmail.com>
2022-03-28 15:12:26 +0200

    add data aug

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -129,6 +129,8 @@ class ConcatTopPatchDataset(BaseDataset):
         if train:
             self.transform = transforms.Compose(
                 [
+                    transforms.RandomHorizontalFlip(),
+                    transforms.RandomVerticalFlip(),
                     transforms.Normalize(
                         mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
                     ),
