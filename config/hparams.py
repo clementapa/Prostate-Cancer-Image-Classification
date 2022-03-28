dd4cf33f2da39edf48d8d8cc7eed5fa1287fb71d
younesbelkada <younesbelkada@gmail.com>
2022-03-28 10:23:44 +0200

    try resnet152

--- config/hparams.py
+++ config/hparams.py
@@ -119,7 +119,7 @@ class CallbacksParams:
 
 @dataclass
 class NetworkClassificationParams:
-    feature_extractor_name: str = "tresnet_xl_448"
+    feature_extractor_name: str = "resnet_152"
     network_name: str = "SimpleModel"
 
 
