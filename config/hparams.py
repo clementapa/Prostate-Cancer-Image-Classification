762501769eabce7b1e7d32ac0c44410f43cd57e2
Clement Apavou <apavou.clement@outlook.fr>
2022-04-01 10:41:07 +0200

    tresnet_xl_448 by default

--- config/hparams.py
+++ config/hparams.py
@@ -119,7 +119,7 @@ class CallbacksParams:
 
 @dataclass
 class NetworkClassificationParams:
-    feature_extractor_name: str = "resnet101"
+    feature_extractor_name: str = "tresnet_xl_448"
     network_name: str = "SimpleModel"
 
 
